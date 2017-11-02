0a48ee6b335b98e5ac127196a17efbb92ed7d595
StephenMcGuckinDAT <stephendmcguckin@gmail.com>
2017-11-02 02:06:31 -0700

    .hyper.js

--- .hyper.js
+++ .hyper.js
@@ -1,84 +1,146 @@
-doc = document.body;
+// Future versions of Hyper may add additional config options,
+// which will not automatically be merged into this file.
+// See https://hyper.is#cfg for all currently supported options.
 
-var table = document.createElement("table");
-var head = document.createElement("thead");
-var body = document.createElement("tbody");
-var elems = [];
-var currentr = 1;
-var currentc = 1;
+module.exports = {
+  config: {
+    // Choose either "stable" for receiving highly polished,
+    // or "canary" for less polished but more frequent updates
+    updateChannel: 'stable',
 
-head.appendChild(document.createElement("tr"));
-table.appendChild(head);
-table.appendChild(body);
-table.style.border = "1px solid";
+    // default font size in pixels for all tabs
+    fontSize: 14,
 
-for(var i = 1; i < 5; i++)
-{
-    var row = document.createElement("th");
-    row.textContent = "Header " + i.toString(10);
-    row.style.border = "1px solid";
-    head.appendChild(row);
-}
+    // font family with optional fallbacks
+    fontFamily: 'MesloLGM Nerd Font, Menlo, "DejaVu Sans Mono", Consolas, "Lucida Console", monospace',
 
-for(var i = 1; i < 4; i++)
-{
-    var row = document.createElement("tr");
-    var rows = [];
-    for(var j = 1; j < 5; j++)
-    {
-        var cell = document.createElement("td");
-        cell.textContent = j + ", " + i;
-        cell.style.border = "1px solid";
-        row.appendChild(cell);
-        rows.push(cell);
-    }
-    body.appendChild(row);
-    elems.push(rows);
-}
+    // terminal cursor background color and opacity (hex, rgb, hsl, hsv, hwb or cmyk)
+    cursorColor: 'rgba(248,28,229,0.8)',
 
-function move(row, col) {
-    if(row > 3 || col > 4 || row < 1 || col < 1)
-        return;
+    // `BEAM` for |, `UNDERLINE` for _, `BLOCK` for █
+    cursorShape: 'BLOCK',
 
-    elems[currentr - 1][currentc - 1].style.border = "1px solid";
-    elems[row - 1][col - 1].style.border = "3px solid";
-    currentr = row;
-    currentc = col;
+    // set to true for blinking cursor
+    cursorBlink: false,
+
+    // color of the text
+    foregroundColor: '#fff',
+
+    // terminal background color
+    backgroundColor: '#000',
+
+    // border color (window, tabs)
+    borderColor: '#333',
+
+    // custom css to embed in the main window
+    css: '',
+
+    // custom css to embed in the terminal window
+    termCSS: '',
+
+    // set to `true` (without backticks) if you're using a Linux setup that doesn't show native menus
+    // default: `false` on Linux, `true` on Windows (ignored on macOS)
+    showHamburgerMenu: '',
+
+    // set to `false` if you want to hide the minimize, maximize and close buttons
+    // additionally, set to `'left'` if you want them on the left, like in Ubuntu
+    // default: `true` on windows and Linux (ignored on macOS)
+    showWindowControls: '',
+
+    // custom padding (css format, i.e.: `top right bottom left`)
+    padding: '12px 14px',
+
+    // the full list. if you're going to provide the full color palette,
+    // including the 6 x 6 color cubes and the grayscale map, just provide
+    // an array here instead of a color map object
+    colors: {
+      black: '#000000',
+      red: '#ff0000',
+      green: '#33ff00',
+      yellow: '#ffff00',
+      blue: '#0066ff',
+      magenta: '#cc00ff',
+      cyan: '#00ffff',
+      white: '#d0d0d0',
+      lightBlack: '#808080',
+      lightRed: '#ff0000',
+      lightGreen: '#33ff00',
+      lightYellow: '#ffff00',
+      lightBlue: '#0066ff',
+      lightMagenta: '#cc00ff',
+      lightCyan: '#00ffff',
+      lightWhite: '#ffffff'
+    },
+
+    // the shell to run when spawning a new session (i.e. /usr/local/bin/fish)
+    // if left empty, your system's login shell will be used by default
+    //
+    // Windows
+    // - Make sure to use a full path if the binary name doesn't work
+    // - Remove `--login` in shellArgs
+    //
+    // Bash on Windows
+    // - Example: `C:\\Windows\\System32\\bash.exe`
+    //
+    // Powershell on Windows
+    // - Example: `C:\\WINDOWS\\System32\\WindowsPowerShell\\v1.0\\powershell.exe`
+    shell: '',
+
+    // for setting shell arguments (i.e. for using interactive shellArgs: ['-i'])
+    // by default ['--login'] will be used
+    shellArgs: ['--login'],
+
+    // for environment variables
+    env: {},
+
+    // set to false for no bell
+    bell: 'SOUND',
+
+    // if true, selected text will automatically be copied to the clipboard
+    copyOnSelect: false
+
+    // if true, on right click selected text will be copied or pasted if no
+    // selection is present (true by default on Windows)
+    // quickEdit: true
+
+    // URL to custom bell
+    // bellSoundURL: 'http://example.com/bell.mp3',
+
+    // for advanced config flags please refer to https://hyper.is/#cfg
+  },
+
+  // a list of plugins to fetch and install from npm
+  // format: [@org/]project[#version]
+  // examples:
+  //   `hyperpower`
+  //   `@company/project`
+  //   `project#1.0.1`
+  plugins: [
+    'hyper-snazzy',
+    'hypercwd',
+    'hyper-blink',
+    'hyper-statusline',
+    // 'hyper-tabs-enhanced',
+    // 'hyper-solarized-dark',
+    'hyper-chesterish'
+    // 'hyper-zenburn'    
+    // 'hypersolar-dark'
+    // 'hyper-star-wars'
+  ],
+
+  hyperStatusLine: {
+    dirtyColor: 'salmon',
+    headColor: 'ivory',
+    footerTransparent: false
+  },
+
+  // in development, you can create a directory under
+  // `~/.hyper_plugins/local/` and include it here
+  // to load it and avoid it being `npm install`ed
+  localPlugins: [],
+
+  keymaps: {
+    // Example
+    // 'window:devtools': 'cmd+alt+o',
+  }
 };
-
-function appendButton(name, action)
-{
-    var button = document.createElement("button");
-    button.textContent = name;
-    button.style.margin = "5px";
-    doc.appendChild(button);
-    button.addEventListener("click", action);
-}
-
-function up() { 
-    move(currentr - 1, currentc);
-};
-
-function down() { 
-    move(currentr + 1, currentc);
-};
-
-function left() {
-        move(currentr, currentc - 1);
-};
-
-function right() { 
-    move(currentr, currentc + 1);
-};
-
-function mark() {
-    elems[currentr - 1][currentc - 1].style.background = "yellow";
-}
-
-document.body.appendChild(table);
-move(1, 1);
-appendButton("up", up);
-appendButton("down", down);
-appendButton("left", left);
-appendButton("right", right);
-appendButton("Mark Cell", mark);
